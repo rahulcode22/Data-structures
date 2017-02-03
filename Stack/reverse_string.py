8dee915c6bd7c8d8557b1b721339e94129072b68
Rahul <rahulrock0082@gmail.com>
2017-02-03 20:03:34 +0530

    Create reverse_string.py

---
+++ Stack/reverse_string.py
@@ -0,0 +1,35 @@
+#python program to print reverse of a string using stack
+#Function to create an empty stack
+def createstack():
+    stack=[]
+    return stack
+#function to get the length of the stack
+def length(stack):
+    return len(stack)
+#stack is empty if size is zero
+def isempty(stack):
+    return size(stack)==0
+def push(item,stack):
+    stack.append(item)
+def pop(stack):
+    return stack.pop()
+#A stack based function to print reverse of string
+def reverse(string):
+    n=len(string)
+    #create an empty stack
+    stack=createstack()
+    #pushing alll characters of string to stack
+    for i in range(n):
+        push(stack,string[i])
+    #make the string empty
+    string=""
+    #now pop all the characters from the string to string
+    for i in range(n):
+        string +=pop(stack)
+    return string
+#test above function
+string="Python"
+string =reverse(string)
+print ("reversed string" +string)
+        
+        
